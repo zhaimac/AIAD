321e2f5b0cf2fdef2786e9e222ba7da8ce6964c9
zhai <zhaimac@gmail.com>
2018-07-17 20:12:57 -0500

    add a compoent

---
+++ src/components/Otherc.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hello">
+      <v-select :items="items"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Otherc',
+  data: () => ({
+    items: ['a','b', 'c']
+  })
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Otherc1/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Otherc1 from  '@/components/Otherc.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
+    Otherc1
   }
 }
 </script>
